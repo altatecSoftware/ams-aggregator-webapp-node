04ca6a874329aac8d05e018ebb398d71a880e3d1
SaulRuizNegrete <saul53141@gmail.com>
2023-07-11 12:23:07 -0600

    initial configurations

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express, { Application } from 'express'
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.SERVER_PORT ?? '8080'
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+})
